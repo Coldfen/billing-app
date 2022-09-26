625f99e0659ae49380c64e9fb552a46d7d2018f5
Coldfen <SanekSnake82@gmail.com>
2022-09-26 16:49:15 +0300

    create header menu and tariff list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import {ProductsListComponent} from "./components/products-list/prooducts-list.c
     PurchaseHistoryComponent,
     AppComponent,
     ProductComponent,
-    ProductsListComponent
+    ProductsListComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,6 +1,16 @@
+export interface ITarif {
+	creationDate: string;
+	tarifName: string;
+	price: string;
+	messageLimit: number;
+	dateStart: string;
+	dateEnd: string;
+}
+
 export interface User {
   email: string
   password: string
   userId?: string
   auth: boolean
 }
+
